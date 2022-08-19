cebe391b7ebab54215ac4d05e8cfab752fd12245
Judith van Leersum <judithvleersum@gmail.com>
2022-08-19 13:24:36 +0200

    Updated pressable and background color of the entire app

--- components/CategoryTile.js
+++ components/CategoryTile.js
@@ -1,10 +1,12 @@
-import { View, Text, StyleSheet, Pressable } from "react-native";
+import { View, Text, StyleSheet, Pressable, Platform } from "react-native";
 
 const CategoryTile = ({ title, color }) => {
   return (
-    <View style={[styles.container, { backgroundColor: color }]}>
-      <Pressable android_ripple={{color: 'white'}} style={({pressed}) => {pressed && styles.pressedItem}}>
+    <View style={styles.outerContainer}>
+      <Pressable android_ripple={{color: 'white'}} style={({pressed}) => [styles.button, pressed && styles.pressedItem]}>
+        <View style={[styles.innerContainer, { backgroundColor: color }]}>
         <Text style={styles.title}>{title}</Text>
+        </View>
       </Pressable>
     </View>
   );
@@ -13,26 +15,34 @@ const CategoryTile = ({ title, color }) => {
 export default CategoryTile;
 
 const styles = StyleSheet.create({
-  container: {
+  outerContainer: {
     flex: 1,
     width: 150,
     height: 150,
-    justifyContent: "center",
-    alignItems: "center",
     margin: 16,
-    padding: 8,
     borderRadius: 6,
     elevation: 4,
     shadowColor: "black",
     shadowOffset: { width: 0, height: 0 },
     shadowOpacity: 0.25,
     shadowRadius: 3,
+    overflow: Platform.OS === 'android' ? "hidden" : "visible"
+  },
+  button: {
+    flex: 1
   },
   pressedItem: {
     opacity: 0.5
   },
+  innerContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: "center",
+    padding: 16,
+    borderRadius: 6,
+  },
   title: {
     fontWeight: "bold",
-  },
-
+    fontSize: 18
+  }
 });
