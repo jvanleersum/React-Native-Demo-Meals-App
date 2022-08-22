33bcf6e10280e9557ef96745f103d11b03ec2477
Judith van Leersum <judithvleersum@gmail.com>
2022-08-22 10:07:59 +0200

    Added text and button if there are no favorites yet

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,18 +1,39 @@
+import { View, Text, StyleSheet, Button } from "react-native";
 import { useContext } from "react";
 
 import FavoritesContext from "../store/context/favorites-context";
 import MealsList from "../components/MealsList/MealsList";
 import { MEALS } from "../data/dummy-data";
 
-const FavoritesScreen = () => {
+const FavoritesScreen = ({navigation}) => {
   const favCtx = useContext(FavoritesContext);
-  const favIds = favCtx.ids;
-  const favMeals = MEALS.filter(meal => favIds.includes(meal.id))
+  const favMeals = MEALS.filter((meal) => favCtx.ids.includes(meal.id));
 
-  return <MealsList meals={favMeals} />
-  
-}
+  const allMealsHandler = () => {
+    navigation.navigate("AllCategories")
+  }
+
+  if (favMeals.length === 0) {
+    return <View style={styles.container}>
+      <Text style={styles.noFavText}>You have not selected any favorites yet</Text>
+      <Button title="Explore meals!" onPress={allMealsHandler} />
+    </View>;
+  }
+
+  return <MealsList meals={favMeals} />;
+};
 
 export default FavoritesScreen;
 
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    margin: 16
+  },
+  noFavText: {
+    fontSize: 16,
+    fontStyle: 'italic',
+    marginVertical: 50
+  }
+})
